2c14e081e3e4012cbfcd4e9bc5bdab4f3e0e85af
Martin <office@martinpagels.de>
2020-07-22 14:46:05 +0200

    add queryselectors

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 "use strict";
 
-// get all main elements
+// get all main elements & buttons
 var mainIndex = document.querySelector('.main_index');
 var mainBookmark = document.querySelector('.main__bookmark');
 var mainCreate = document.querySelector('.main__create');
 var mainProfile = document.querySelector('.main__profile');
+var navButtonHome = document.querySelector('.btn-home');
+var navButtonSaved = document.querySelector('.btn-bookmarks');
+var navButtonAdd = document.querySelector('btn-add');
+var navButtonProfile = document.querySelector('btn-profile');
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,13 @@
-// get all main elements
+// get all main elements & buttons
 
 const mainIndex = document.querySelector('.main_index')
 const mainBookmark = document.querySelector('.main__bookmark')
 const mainCreate = document.querySelector('.main__create')
 const mainProfile = document.querySelector('.main__profile')
+
+const navButtonHome = document.querySelector('.btn-home')
+const navButtonSaved = document.querySelector('.btn-bookmarks')
+const navButtonAdd = document.querySelector('btn-add')
+const navButtonProfile = document.querySelector('btn-profile')
+
+// Events
