825611ed328e4c33ef093d5841f6e971b48cc4c9
Martin <office@martinpagels.de>
2020-07-22 23:39:30 +0200

    add img constants back

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
+// constants
+const HOME_ACTIVE = 'images/buttons/round_home_black_48dp.png'
+const HOME_INACTIVE = 'images/buttons/round_home_outline_48dp.png'
+const SAVED_ACTIVE = 'images/buttons/round_bookmarks_black_48dp.png'
+const SAVED_INACTIVE = 'images/buttons/round_bookmarks_outline_48dp.png'
+const ADD_ACTIVE = 'images/buttons/round_add_box_black_48dp.png'
+const ADD_INACTIVE = 'images/buttons/round_add_box_outline_48dp.png'
+const PROFILE_ACTIVE = 'images/buttons/round_account_box_black_48dp.png'
+const PROFILE_INACTIVE = 'images/buttons/round_account_box_outline_48dp.png'
+
 // main elements
 const mainIndex = document.querySelector('.main__index')
 const mainBookmark = document.querySelector('.main__bookmark')
