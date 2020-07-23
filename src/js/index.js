901c51c693bae6dd96f3a2923bf40790db2f8ce6
Martin <office@martinpagels.de>
2020-07-23 23:56:55 +0200

    Move from logic to form.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,9 @@
-import { get } from './utility'
 import navInit from './nav'
 import bookmarkLogic from './bookmark'
 import cardLogic from './card'
+import formLogic from './form'
 
 navInit()
 bookmarkLogic()
 cardLogic()
-
-// form
-const formButtonSubmit = get('.card__button--submit')
-
-// Form
-const createForm = get('form')
-
-formButtonSubmit.addEventListener('click', (event) => {
-  event.preventDefault()
-  get('textarea[name=question]').value = ''
-  get('textarea[name=answer]').value = ''
-  get('input[name=tags]').value = ''
-})
+formLogic()
