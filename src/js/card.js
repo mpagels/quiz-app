5145ded5f5190ba02066cdff9499e60b987d253d
Martin <office@martinpagels.de>
2020-07-24 00:02:35 +0200

    better HOF function logic

--- src/js/card.js
+++ src/js/card.js
@@ -11,46 +11,29 @@ export default function () {
   const answerCard2 = get('.answer__card-2')
   const answerCard3 = get('.answer__card-3')
 
-  showAnswerCard1.addEventListener('click', forCard(1))
-  showAnswerCard2.addEventListener('click', forCard(2))
-  showAnswerCard3.addEventListener('click', forCard(3))
+  showAnswerCard1.addEventListener(
+    'click',
+    forCard(showAnswerCard1, answerCard1)
+  )
+  showAnswerCard2.addEventListener(
+    'click',
+    forCard(showAnswerCard2, answerCard2)
+  )
+  showAnswerCard3.addEventListener(
+    'click',
+    forCard(showAnswerCard3, answerCard3)
+  )
 
-  function forCard(number) {
-    if (number === 1) {
-      return () => {
-        if (showAnswerCard1.classList.contains('card__button--show-answer')) {
-          answerCard1.classList.remove('hidden')
-          showAnswerCard1.classList.remove('card__button--show-answer')
-          showAnswerCard1.classList.add('card__button--hide-answer')
-        } else {
-          answerCard1.classList.add('hidden')
-          showAnswerCard1.classList.add('card__button--show-answer')
-          showAnswerCard1.classList.remove('card__button--hide-answer')
-        }
-      }
-    } else if (number === 2) {
-      return () => {
-        if (showAnswerCard2.classList.contains('card__button--show-answer')) {
-          answerCard2.classList.remove('hidden')
-          showAnswerCard2.classList.remove('card__button--show-answer')
-          showAnswerCard2.classList.add('card__button--hide-answer')
-        } else {
-          answerCard2.classList.add('hidden')
-          showAnswerCard2.classList.add('card__button--show-answer')
-          showAnswerCard2.classList.remove('card__button--hide-answer')
-        }
-      }
-    } else if (number === 3) {
-      return () => {
-        if (showAnswerCard3.classList.contains('card__button--show-answer')) {
-          answerCard3.classList.remove('hidden')
-          showAnswerCard3.classList.remove('card__button--show-answer')
-          showAnswerCard3.classList.add('card__button--hide-answer')
-        } else {
-          answerCard3.classList.add('hidden')
-          showAnswerCard3.classList.add('card__button--show-answer')
-          showAnswerCard3.classList.remove('card__button--hide-answer')
-        }
+  function forCard(button, answerCard) {
+    return () => {
+      if (button.classList.contains('card__button--show-answer')) {
+        answerCard.classList.remove('hidden')
+        button.classList.remove('card__button--show-answer')
+        button.classList.add('card__button--hide-answer')
+      } else {
+        answerCard.classList.add('hidden')
+        button.classList.add('card__button--show-answer')
+        button.classList.remove('card__button--hide-answer')
       }
     }
   }
