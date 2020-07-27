1a27647248005ed015b42cd5e9b28c8390c03954
Martin <office@martinpagels.de>
2020-07-28 00:19:15 +0200

    Update

--- src/js/index.js
+++ src/js/index.js
@@ -2,40 +2,14 @@ import bookmarkLogic from './bookmark'
 import cardLogic from './card'
 import formLogic from './form'
 import navInit from './nav'
+import dataInit from './card_content'
 
-document.addEventListener(
-  'DOMContentLoaded',
-  function () {
-    // all my imported functions for the first time
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
     navInit()
+    dataInit()
     bookmarkLogic()
     cardLogic()
     formLogic()
-    const callback = function (mutationsList) {
-      for (let i = 0, len = mutationsList.length; i < len; i++) {
-        if (mutationsList[i].type == 'childList') {
-          // all my imported functions for the second time
-          navInit()
-          bookmarkLogic()
-          cardLogic()
-          formLogic()
-          break
-        }
-      }
-    }
-
-    const observer = new MutationObserver(callback)
-    const config = { childList: true, subtree: false }
-    observer.observe(document.getElementById('root'), config)
-  },
-  false
-)
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   setTimeout(() => {
-//     navInit()
-//     bookmarkLogic()
-//     cardLogic()
-//     formLogic()
-//   }, 100)
-// })
+  }, 100)
+})
