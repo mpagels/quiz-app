adbfa43de3f2238f22afc4663aee8981e5f2cbee
Martin <office@martinpagels.de>
2020-07-28 00:47:04 +0200

    Change filename

---
+++ src/js/card-create.js
@@ -0,0 +1,45 @@
+import { get } from './utility'
+import { CARD_DATA } from './card-data'
+
+const cardSection = document.querySelector('.main__index')
+
+export default function cardContent() {
+  CARD_DATA.forEach(buildCardwith)
+}
+
+// Helper functions
+function buildCardwith(data) {
+  const el = document.createElement('section')
+  el.className = 'card'
+  el.innerHTML = buildInnerHTML(data)
+  buildTags(data, get('ul', el))
+  cardSection.appendChild(el)
+}
+
+function buildInnerHTML(data) {
+  return `<button class="card__bookmark${data.bookmarked}"></button>
+    <section class="card__content">
+      <section class="card__question">
+        ${data.question}
+        <span>
+          <ul>
+          </ul>
+        </span>
+      </section>
+      <section class="answer hidden">
+      ${data.answer}
+      </section>
+      <section class="card__button">
+        <button class="card__button--show-answer"></button>
+      </section>
+    </section>`
+}
+
+function buildTags(data, ulElement) {
+  data.tags.forEach((tag) => {
+    const li = document.createElement('li')
+    li.textContent = tag
+    li.className = 'tags'
+    ulElement.appendChild(li)
+  })
+}
---
+++ src/js/card-data.js
@@ -0,0 +1,16 @@
+export const CARD_DATA = [
+  {
+    question: 'Do all HTML tags come in a pair?',
+    answer:
+      'No, there are single HTML tags that do not need a closing tag. Examples are the <img> tag and <br> tags.',
+    tags: ['HTML', 'Basics'],
+    bookmarked: '--active',
+  },
+  {
+    question: 'What are style sheets?',
+    answer:
+      'Style sheets enable you to build consistent, transportable, and well-defined style templates. These templates can be linked to several different web pages, making it easy to maintain and change the look and feel of all the web pages within site.',
+    tags: ['HTML', 'CSS', 'Basics'],
+    bookmarked: '--inactive',
+  },
+]
