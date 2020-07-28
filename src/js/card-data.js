7883c8bb0c9d57bc5369b58ada6a42ecedc51434
Martin <office@martinpagels.de>
2020-07-28 23:04:37 +0200

    Correct property declaration

--- src/js/card-data.js
+++ src/js/card-data.js
@@ -1,6 +1,6 @@
 export const CARD_DATA = [
   {
-    id = 1,
+    id: 1,
     question: 'Do all HTML tags come in a pair?',
     answer:
       'No, there are single HTML tags that do not need a closing tag. Examples are the <img> tag and <br> tags.',
@@ -8,7 +8,7 @@ export const CARD_DATA = [
     bookmarked: true,
   },
   {
-    id = 2,
+    id: 2,
     question: 'What are style sheets?',
     answer:
       'Style sheets enable you to build consistent, transportable, and well-defined style templates. These templates can be linked to several different web pages, making it easy to maintain and change the look and feel of all the web pages within site.',
