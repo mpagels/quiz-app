d26957a0ff0d01c36362ecfb556c3287066fa368
Martin <office@martinpagels.de>
2020-07-28 17:02:06 +0200

    Add insert new card in CARD_DATA Array

--- src/js/form.js
+++ src/js/form.js
@@ -1,12 +1,20 @@
 import { get } from './utility'
+import { CARD_DATA } from './card-data'
 
 export default function () {
   const form = get('form')
 
   form?.addEventListener('submit', (event) => {
     event.preventDefault()
-    get('textarea[name=question]').value = ''
-    get('textarea[name=answer]').value = ''
-    get('input[name=tags]').value = ''
+    createCard(form.question.value, form.answer.value, form.tags.value)
+    form.reset()
+  })
+}
+
+function createCard(question, answer, tags) {
+  CARD_DATA.push({
+    question: question,
+    answer: answer,
+    tags: tags.split(',').map((tag) => tag.trim()),
   })
 }
