d0102f8b1bfc7e72ad5ecf07a1a421eb13873b4b
Martin <office@martinpagels.de>
2020-07-28 23:02:38 +0200

    Add id property

--- src/js/card-data.js
+++ src/js/card-data.js
@@ -1,5 +1,6 @@
 export const CARD_DATA = [
   {
+    id = 1,
     question: 'Do all HTML tags come in a pair?',
     answer:
       'No, there are single HTML tags that do not need a closing tag. Examples are the <img> tag and <br> tags.',
@@ -7,6 +8,7 @@ export const CARD_DATA = [
     bookmarked: true,
   },
   {
+    id = 2,
     question: 'What are style sheets?',
     answer:
       'Style sheets enable you to build consistent, transportable, and well-defined style templates. These templates can be linked to several different web pages, making it easy to maintain and change the look and feel of all the web pages within site.',
