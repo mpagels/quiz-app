483f79c04cd55da3fa8f7e20d995b706bd5f519c
Martin <office@martinpagels.de>
2020-07-29 09:09:24 +0200

    Create file

---
+++ src/js/anwer-toggleLogic.js
@@ -0,0 +1,22 @@
+export function toggleAnswer(card) {
+  const buttonAnswer = card.querySelector('[class*="card__button--"]')
+  const answer = card.querySelector('.answer')
+  buttonAnswer.addEventListener(
+    'click',
+    showAnswerForCard(buttonAnswer, answer)
+  )
+}
+
+function showAnswerForCard(button, answerCard) {
+  return () => {
+    if (button.classList.contains('card__button--show-answer')) {
+      answerCard?.classList.remove('hidden')
+      button?.classList.remove('card__button--show-answer')
+      button?.classList.add('card__button--hide-answer')
+    } else {
+      answerCard?.classList.add('hidden')
+      button?.classList.add('card__button--show-answer')
+      button?.classList.remove('card__button--hide-answer')
+    }
+  }
+}
