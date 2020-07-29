95c9307f6158825979e02ebeb9de57f51b337c1e
Martin <office@martinpagels.de>
2020-07-30 00:18:46 +0200

    Add createElement(fn)

--- src/js/utility.js
+++ src/js/utility.js
@@ -5,3 +5,18 @@ export function get(selector, target = document) {
 export function getAll(selector, target = document) {
   return target.querySelectorAll(selector)
 }
+
+export function createElement({
+  type = 'div',
+  className = '',
+  target = document.body,
+  id = false,
+}) {
+  const el = document.createElement(type)
+  el.className = className
+  if (id) {
+    el.setAttribute('data-id', id)
+  }
+  target.appendChild(el)
+  return el
+}
