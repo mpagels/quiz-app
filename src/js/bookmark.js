f835ef1dd54bc26f1042d49a8375787b929c7159
Martin <office@martinpagels.de>
2020-07-29 09:10:11 +0200

    Add write toogle data bookmark property logic

--- src/js/bookmark.js
+++ src/js/bookmark.js
@@ -1,15 +1,25 @@
-import { get, getAll } from './utility'
+import { CARD_DATA } from './card-data'
+import { cardLogic } from './card-create'
 
-export default function () {
-  const bookmarks = getAll('[class*="card__bookmark--"]')
-  bookmarks.forEach((bookmark, event) => {
-    bookmark.addEventListener('click', toggleBookmark(event))
-  })
+export function bookmarkLogic(card) {
+  const bookmark = card.querySelector('[class*="card__bookmark--"]')
+  bookmark.addEventListener('click', toggleBookmark)
+}
 
-  function toggleBookmark(event) {
-    return (event) => {
-      event.target.classList.toggle('card__bookmark--active')
-      event.target.classList.toggle('card__bookmark--inactive')
-    }
+function toggleBookmark(event) {
+  event.target.classList.toggle('card__bookmark--active')
+  event.target.classList.toggle('card__bookmark--inactive')
+  const id = event.target.dataset.id
+  changeBookmark(id)
+  if (event.target.parentElement.parentElement.className === 'main__bookmark') {
+    cardLogic(true)
   }
+}
+
+function changeBookmark(id) {
+  const data = { ...CARD_DATA[id - 1] }
+  data.bookmarked = data.bookmarked
+    ? (data.bookmarked = false)
+    : (data.bookmarked = true)
+  CARD_DATA[id - 1] = data
 }
