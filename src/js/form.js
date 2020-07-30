c6b2499929c6303c4ce0f41250840878156fdf0f
Martin <office@martinpagels.de>
2020-07-31 00:42:29 +0200

    Change for DataInterface

--- src/js/form.js
+++ src/js/form.js
@@ -1,22 +1,15 @@
 import { get } from './utility'
-import { CARD_DATA } from './card-data'
 
-export default function () {
+export default function ({ insertCard }) {
   const form = get('form')
 
   form?.addEventListener('submit', (event) => {
     event.preventDefault()
-    createCardData(form.question.value, form.answer.value, form.tags.value)
+    insertCard({
+      question: form.question.value,
+      answer: form.answer.value,
+      tags: form.tags.value,
+    })
     form.reset()
-  })
-}
-
-function createCardData(question, answer, tags) {
-  CARD_DATA.push({
-    id: CARD_DATA.length + 1,
-    question: question,
-    answer: answer,
-    bookmarked: false,
-    tags: tags.split(',').map((tag) => tag.trim()),
   })
 }
